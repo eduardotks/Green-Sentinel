75f4ca0a63678e99a8e61865fab0a923cbbebfb7
rafaelfmg <rafaelf.fmg@gmail.com>
2022-02-17 18:05:48 -0300

    inclusão crud sensores

--- Python/app/models.py
+++ Python/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Sensor(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    model = models.CharField(max_length=20)
+    potency = models.CharField(max_length=5)
+    duration = models.IntegerField()
+    serial = models.CharField(max_length=20)
+    brand = models.CharField(max_length=20)
